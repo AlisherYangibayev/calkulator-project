5468b1e6df479b282813d4b16e57ba1bf097c239
hp <hp@LAPTOP-BNR4JGAF>
2023-03-25 20:52:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_repo_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+        }
+    }
+}
