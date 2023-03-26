e86cad919121bac94ca269c118f56484d26db628
Alisher.Y <cank072014@gmail.com>
2023-03-26 18:28:28 +0300

    feat..add user inputs

--- Program.cs
+++ Program.cs
@@ -11,6 +11,12 @@ namespace my_repo_project
         static void Main(string[] args)
         {
             Console.WriteLine("Hesap Makinesi");
+            Console.WriteLine("Bir sayi giriniz");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Ikinci Sayiyi Giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+
 
         }
     }
