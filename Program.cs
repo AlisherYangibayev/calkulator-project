c86aa3582f80be55161d25b2532728947bf7978c
Alisher.Y123 <cank072014@gmail.com>
2023-03-26 20:42:19 +0300

    fast

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace my_repo_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hesap Makinesi");
+            Console.WriteLine ("Hesap Makinesi");
 
             Console.WriteLine("Bir sayi giriniz");
             double sayi1 = int.Parse(Console.ReadLine());
@@ -20,7 +20,7 @@ namespace my_repo_project
            string islem = (Console.ReadLine());
 
             Console.WriteLine("Bir sayi giriniz");
-            double sayi = int.Parse(Console.ReadLine());
+            double sayi2 = int.Parse(Console.ReadLine());
 
             double sonuc = 0;
 
@@ -28,30 +28,14 @@ namespace my_repo_project
            
             switch(islem)
             {
-                case "+":
-                    sonuc= sayi1+ sayi;
-                   
-                    break;
-                case "-":
-                    sonuc = sayi1 - sayi;
-                   
-                    break;
-                case "*":
-                    sonuc = sayi1 * sayi;
-                    
-                    break;
-                case "/":
-                    sonuc = sayi1 / sayi;
-                    
-                    break;
-                case "%":
-                    sonuc = sayi1 %sayi;
-                    
-                    break;
+                case "+": sonuc = sayi1+sayi2;   break;
+                case "-": sonuc = sayi1-sayi2;   break;
+                case "*": sonuc = sayi1*sayi2;   break;
+                case "/": sonuc = (sayi1/sayi2); break;
+                case "%": sonuc = (sayi1%sayi2); break;
+                 // Console.WriteLine("{0} {1} {2}={3}");
 
-                   // Console.WriteLine("{0} {1} {2}={3}");
-
-                    Console.WriteLine(sayi1 +" "+ islem+" "+sayi+ " "+ sonuc  );    
+              Console.WriteLine(sayi1 +" "+ islem+" "+sayi2 + " "+ sonuc );    
 
 
 
