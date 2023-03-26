b8cfd99eb8e1f56d8988c2e728673aaea5d2fa21
Alisher.Y <cank072014@gmail.com>
2023-03-26 18:40:50 +0300

    fix:change verable type int to string

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,12 +17,16 @@ namespace my_repo_project
             int sayi1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine(" Islem Seciniz (+) (-) (*) (/) (%) :");
-            int islem = int.Parse(Console.ReadLine());
+           string islem = (Console.ReadLine());
 
             Console.WriteLine("Bir sayi giriniz");
             int sayi = int.Parse(Console.ReadLine());
 
 
+
+
+           
+            
 
 
         }
